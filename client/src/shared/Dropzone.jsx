4f561ffe5d7f1eb9b83193a367d26e3dbb414cd1
Maev4l <maeval.nightingale@gmail.com>
2023-05-18 12:31:34 +0200

    Increase file count limit

--- client/src/shared/Dropzone.jsx
+++ client/src/shared/Dropzone.jsx
@@ -1,25 +1,28 @@
 import { DropzoneArea } from 'mui-file-dropzone';
 
+const FILES_LIMIT = 20;
+const MAX_FILE_SIZE = FILES_LIMIT * 1024 * 1024; // Max 20Mb
+
 const Dropzone = ({ onChange, acceptedFiles, dropzoneText, showPreview }) =>
   showPreview ? (
     <DropzoneArea
       onChange={onChange}
       acceptedFiles={acceptedFiles}
-      filesLimit={10}
+      filesLimit={FILES_LIMIT}
       useChipsForPreview={false}
       showPreviewsInDropzone
       showFileNames
       dropzoneText={dropzoneText}
-      maxFileSize={15 * 1024 * 1024} // Max 10Mb
+      maxFileSize={MAX_FILE_SIZE} // Max 20Mb
     />
   ) : (
     <DropzoneArea
       onChange={onChange}
       acceptedFiles={acceptedFiles}
-      filesLimit={10}
+      filesLimit={FILES_LIMIT}
       useChipsForPreview
       dropzoneText={dropzoneText}
-      maxFileSize={15 * 1024 * 1024} // Max 10Mb
+      maxFileSize={MAX_FILE_SIZE} // Max 20Mb
     />
   );
 export default Dropzone;
